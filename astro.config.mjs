4722d93adee370bc0e98627bc3b2633df50c28db
Kaliel Williamson <kwilliamson35@gatech.edu>
2026-08-11 20:39:38 -0400

    Connect portfolio to custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	// Remove `base` and set `site` to the selected custom domain once DNS is connected.
-	site: 'https://kalielwilliamson.github.io',
-	base: '/kalielwilliamson.com',
+	site: 'https://kalielwilliamson.com',
 });
